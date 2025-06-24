79a3d9746b42ddcbeb832c80d79a48c05510b8a1
rahulpancanter <smaritikumari166@gmail.com>
2025-06-24 16:00:04 +0530

    Update PanData.js

--- models/PanData.js
+++ models/PanData.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const panDataSchema = new mongoose.Schema({
-  pannumber: { type: String, required: true },
-  acknowledgmentNo: { type: String, required: true },
-  fullName: { type: String, required: true },
-  email: { type: String, required: true },
-  status: { type: String, required: true },
-  pdfFileName: { type: String }
+const panSchema = new mongoose.Schema({
+  acknowledgmentNo: String,
+  pannumber: String,
+  fullName: String,
+  email: String,
+  status: String,
+  pdfFileName: String,
 }, { timestamps: true });
 
-module.exports = mongoose.model('PanData', panDataSchema);
+module.exports = mongoose.model('PanData', panSchema);
